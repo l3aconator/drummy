4147279f40ad228621aa2d34be4c7badd2b31bb4
l3acon <hello@kylebaconcreative.com>
2016-06-30 22:31:34 -0700

    Building Sass framework.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,22 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+var autoprefixer = require('gulp-autoprefixer');
+var csslint = require('gulp-csslint');
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['css']);
+});
+
+gulp.task('css', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+    
+	return gulp.src('src/app.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist'));
 });
